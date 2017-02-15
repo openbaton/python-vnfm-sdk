60c39fe95684c9afba4914e6f24ec6f78e12c23c
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-02-15 15:34:39 +0100

    imp: add log

--- vnfm/sdk/AbstractVnfmABC.py
+++ vnfm/sdk/AbstractVnfmABC.py
@@ -398,6 +398,7 @@ class AbstractVnfm(threading.Thread):
     def allocate_resources(self, vnf_record, vim_instances, keys, **kwargs):
         user_data = self.get_user_data()
         user_data.replace("export MONITORING_IP=","export MONITORING_IP=%s" % kwargs.get("monitoringIp"))
+        log.debug("Sending userdata: \n%s" % user_data)
         nfv_message = get_nfv_message(action="ALLOCATE_RESOURCES", vnfr=vnf_record, vim_instances=vim_instances,
                                       user_data=user_data, keys=keys)
         log.debug("Executing ALLOCATE_RESOURCES")
