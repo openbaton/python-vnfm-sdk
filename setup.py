0b8954c7ed82da5feb45ebb06cf9a83583b65b03
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-02-21 17:43:55 +0100

    fix: userdata check if none

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="python-vnfm-sdk",
-    version='3.2.0b3',
+    version='3.2.0b4',
     author="Open Baton",
     author_email="dev@openbaton.org",
     description="The Python version of the Open Baton vnfm-sdk",
