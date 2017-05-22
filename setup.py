0dedfa973b8ab52cf5c3506e0c5dc1bb11fbaa3e
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-05-22 12:49:13 +0200

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="python-vnfm-sdk",
-    version='3.2.0b8',
+    version='3.2.1',
     author="Open Baton",
     author_email="dev@openbaton.org",
     description="The Python version of the Open Baton vnfm-sdk",
